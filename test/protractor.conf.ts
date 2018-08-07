61d97e8b9a51480ca29c32d88cbfc9c4b1bff62e
Walmyr <wlsf82@gmail.com>
2018-08-07 12:53:49 +0200

    create first version of a protractor and typescript project

---
+++ test/protractor.conf.ts
@@ -0,0 +1,31 @@
+import { Config, browser } from 'protractor';
+import { DisplayProcessor, SpecReporter } from 'jasmine-spec-reporter';
+import SuiteInfo = jasmine.SuiteInfo;
+
+class CustomProcessor extends DisplayProcessor {
+  public displayJasmineStarted(info: SuiteInfo, log: string): string {
+    return `TypeScript ${log}`;
+  }
+}
+
+export let config: Config = {
+  framework: 'jasmine',
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: [
+        '--headless',
+        '--disable-gpu',
+        '--window-size=1280,1024'
+      ]
+    }
+  },
+  baseUrl: 'http://todomvc.com',
+  specs: ['./specs/sample.spec.js'],
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    jasmine.getEnv().addReporter(new SpecReporter({
+      customProcessors: [CustomProcessor],
+    }));
+  },
+};
---
+++ test/specs/sample.spec.ts
@@ -0,0 +1,30 @@
+import * as helper from 'protractor-helper';
+
+import { browser, element, ElementFinder, by } from 'protractor';
+
+describe('protractor with typescript typings', () => {
+  const todoList: ElementFinder = element(by.className('todo-list'));
+  const someText: String = 'Foo';
+
+  beforeEach(() =>  {
+    const newTodoField: ElementFinder = element(by.className('new-todo'));
+
+    browser.get('/examples/react')
+    helper.fillFieldWithTextWhenVisibleAndPressEnter(newTodoField, someText);
+    helper.waitForTextToBePresentInElement(todoList, someText);
+  });
+
+  it('should show an item in the todo list', () => {
+    todoList.getText().then(text => expect(text).toEqual(someText));
+  });
+
+  it('show not show the todo list after clicking toggle all and clearing completed', () => {
+    const toggleAllButton: ElementFinder = element(by.className('toggle-all'));
+    const clearCompletedButton: ElementFinder = element(by.className('clear-completed'));
+
+    helper.clickWhenClickable(toggleAllButton);
+    helper.clickWhenClickable(clearCompletedButton);
+
+    helper.waitForElementNotToBePresent(todoList);
+  });
+});
